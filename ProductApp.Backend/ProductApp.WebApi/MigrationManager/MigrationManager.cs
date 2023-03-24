0e5c42099e28713e4f5a9c5140140cd591d519b6
ProsTuha <tuhalf2002@mail.ru>
2023-03-24 20:49:26 +0300

    Added migration logic while project starts

---
+++ ProductApp.Backend/ProductApp.WebApi/MigrationManager/MigrationManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductApp.Persistence.MigrationManager;
+
+public static class MigrationManager
+{
+    public static WebApplication MigrateDatabase(this WebApplication host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            using (var appContext = scope.ServiceProvider.GetRequiredService<ProductsDbContext>())
+            {
+                try
+                {
+                    appContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+        return host;
+    }
+}
