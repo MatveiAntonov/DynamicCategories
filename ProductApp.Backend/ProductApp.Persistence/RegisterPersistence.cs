69e7ea66d3c08d9484fbe0d2878d9328e6a3d517
ProsTuha <tuhalf2002@mail.ru>
2023-03-24 20:48:25 +0300

    Added registration for di

---
+++ ProductApp.Backend/ProductApp.Persistence/RegisterPersistence.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApp.Domain.Interfaces;
+using ProductApp.Persistence.Repositories;
+
+namespace ProductApp.Persistence
+{
+    public static class RegisterPersistence
+    {
+        public static IServiceCollection ConnectDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ProductsDbContext>(options => {
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped <ICategoryFieldRepository, CategoryFieldRepository>();
+
+            return services;
+        }
+    }
+}
