c1739a1d91b2f1fbf353a1da6ca734ec87c446c7
ProsTuha <tuhalf2002@mail.ru>
2023-03-24 20:47:04 +0300

    Added Context

---
+++ ProductApp.Backend/ProductApp.Persistence/ProductsDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Domain.Entities;
+using ProductApp.Persistence.EntityTypeConfigurations;
+
+namespace ProductApp.Persistence
+{
+    public class ProductsDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryField> CategoryFields { get; set; }
+        public DbSet<ProductField> ProductFields { get; set; }
+
+
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CategoriesConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryFieldsConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductsConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductFieldsConfiguration());
+
+        }
+
+    }
+}
