685f7af00a374b69ce447d4b9a8064c6f8869a7a
ProsTuha <tuhalf2002@mail.ru>
2023-03-24 20:47:27 +0300

    Added configurations for tables

---
+++ ProductApp.Backend/ProductApp.Persistence/EntityTypeConfigurations/CategoryFieldsConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ProductApp.Domain.Entities;
+
+namespace ProductApp.Persistence.EntityTypeConfigurations
+{
+    public class CategoryFieldsConfiguration : IEntityTypeConfiguration<CategoryField>
+    {
+        public void Configure(EntityTypeBuilder<CategoryField> builder)
+        {
+            // id
+            builder.HasKey(cf => cf.Id);
+            builder.Property(cf => cf.Id)
+                .HasColumnName("id");
+
+            // Name
+            builder.Property(cf => cf.Name)
+                .IsRequired()
+                .HasColumnName("name");
+
+            // Description
+            builder.Property(cf => cf.Description)
+                .HasColumnName("description");
+
+            // Categories
+            builder.HasOne(cf => cf.Category)
+            .WithMany(c => c.CategoryFields)
+            .HasForeignKey(cf => cf.CategoryId);
+
+            builder.Property(cf => cf.CategoryId)
+                .HasColumnName("category_id");
+
+            // table name
+            builder.ToTable("category_fields");
+        }
+    }
+}
