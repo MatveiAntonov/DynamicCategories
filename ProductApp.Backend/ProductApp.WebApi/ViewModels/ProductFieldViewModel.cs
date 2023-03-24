ee0c8d6cddaad344989e525e656a7da736187d36
ProsTuha <tuhalf2002@mail.ru>
2023-03-24 20:49:58 +0300

    Added ViewModels for entities

---
+++ ProductApp.Backend/ProductApp.WebApi/ViewModels/ProductFieldViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApp.WebApi.ViewModels
+{
+    public class ProductFieldViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
